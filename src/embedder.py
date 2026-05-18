8999e08d682b7b5e214fa6ae82b75ea48c60fec1
Hema Sameera <addagarlahemasameera@gmail.com>
2026-05-18 10:06:44 +0530

    Remove src folder

--- src/embedder.py
+++
@@ -1,7 +0,0 @@
-from langchain_ollama import OllamaEmbeddings
-from src.config import EMBED_MODEL
-
-
-def get_embedder() -> OllamaEmbeddings:
-    """Returns the embedding model defined in config."""
-    return OllamaEmbeddings(model=EMBED_MODEL)
